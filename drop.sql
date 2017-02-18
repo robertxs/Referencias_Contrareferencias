49470a434ab6432012f243cfe883bfe50621a8b8
cinthyaramos <09-11237@usb.ve>
2017-02-18 11:16:18 -0400

    archivo de drop tablas nuevas

--- drop.sql
+++ drop.sql
@@ -12,5 +12,36 @@ drop table medico_medico_estudios;
 drop table medico_medico;
 drop table medico_institucion;
 drop table medico_especialidad;
+drop table django_migrations;
+
+drop table django_content_type cascade;
+drop table auth_permission cascade;
+drop table auth_group cascade;
+drop table auth_group_permissions;
+drop table auth_user_groups;
+drop table auth_user_user_permissions;
+drop table django_admin_log ;
+drop table django_session;
+
+drop table administrador_inbox;
 drop table administrador_usuario;
-create table administrador_usuario(id varchar(30) primary key, user_id varchar(30), ci varchar(30) unique);
+drop table auth_user;
+drop table auth_user_groups;
+drop table auth_user_user_permissions;
+drop table django_admin_log;
+drop table medico_medico_citas cascade;
+drop table medico_medico_diagnostico cascade;
+drop table medico_medico_informe cascade;
+drop table medico_medico_revision cascade;
+drop table medico_referencia cascade;
+drop table medico_emergencia cascade;
+drop table medico_institucion cascade;
+drop table medico_medico cascade;
+
+drop table paciente_historiadetriaje;
+drop table paciente_paciente;
+drop table administrador_usuario;
+
+
+-- alter table django_content_type ADD column name varchar(30);
+-- create table administrador_usuario(id varchar(30) primary key, user_id varchar(30), ci varchar(30) unique);
